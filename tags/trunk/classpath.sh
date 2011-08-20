c58196371d75a03b2354cd8e44a827c792e548c2
difallah@gmail.com <difallah@gmail.com@8c9255d6-0b70-036a-fcb6-8a999947b34c>
2011-08-20 09:54:41 +0000

    Cleanup tags repository

--- tags/trunk/classpath.sh
+++
@@ -1,6 +0,0 @@
-echo -ne "./bin"
-for i in `ls ./lib/*.jar`
-do
-echo -ne ":$i"
-done
-
--- tags/trunk/trunk/classpath.sh
+++
@@ -1,7 +0,0 @@
-BASEDIR=/Users/Djellel/Documents/workspace
-echo -ne "./bin"
-for i in `ls ./lib/*.jar`
-do
-echo -ne ":$i"
-done
-
